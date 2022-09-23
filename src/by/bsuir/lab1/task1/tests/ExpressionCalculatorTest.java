89ace6b960ad60c8f5b40604c364e0b78f59cc05
Roman Shidlovsky <jagerlion@mail.ru>
2022-09-24 00:11:35 +0300

    Add task1 and task1.tests

--- src/by/bsuir/lab1/task1/tests/ExpressionCalculatorTest.java
+++ src/by/bsuir/lab1/task1/tests/ExpressionCalculatorTest.java
@@ -8,11 +8,11 @@ class ExpressionCalculatorTest {
 
     @Test
     void calculateTestPositiveDouble() {
-        //arrange
+        // arrange
         double x = 2.9;
         double y = 5.7;
         double expected = 3.2155;
-        //act
+        // act
         double actual = ExpressionCalculator.calculate(x, y);
         // assert
         assertEquals(expected, actual, 0.0001);
@@ -20,7 +20,7 @@ class ExpressionCalculatorTest {
 
     @Test
     void calculateTestPositiveAndNegativeDouble() {
-        //arrange
+        // arrange
         double x = -3.33;
         double y = 2.123;
         double expected = -2.9697;
